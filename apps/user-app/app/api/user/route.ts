2066d65530a573741687609a4a120951017888b5
sahil <sahilcodes23@gmail.com>
2024-07-09 18:16:25 +0530

    sadasd

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import { authOptions } from "../../lib/auth";
 
 export const GET = async () => {
+    
     const session = await getServerSession(authOptions);
     if (session.user) {
         return NextResponse.json({
